1dc6e8c667cc96278b0330f0b3314a65bb06ec5f
Mingyu Li <mingyuli19910814@gmail.com>
2022-05-01 21:56:21 +0900

    update README.md

--- README.md
+++ README.md
@@ -1,7 +1,37 @@
 # Introduction
+This repository implements the CNN-based sentiment analysis of [Convolutional Neural Networks for Sentence Classification](https://arxiv.org/abs/1408.5882) in Tensorflow 2.
 
-# How to train
+# Requirements
+```commandline
+conda env create -f environment.yml
 ```
+
+# Train
+
+In this implementation, the words would firstly be converted to 300 dimensional vectors using pretrained word2vec of Google News 300 for transfer learning.
+This transfering is very time-consuming and becomes the bottleneck of the training.
+Therefore, it is necessary to firstly calculate the word vectors and save them to tfrecord.
+Same as the paper, this implementation also uses 10-fold cross validation.
+```commandline
+export NEG_SAMPLE_PATH=<negative_sample_path> # change this to your negative sample file
+export POS_SAMPLE_PATH=<positive_sample_path> # change this to your positive sample file
+export TFRECORD_DIR=<tfrecord_path> # change this to your favorite directory
+export KFOLD=10 # change this to any number
+conda activate tensorflow
+python data_preprocess.py --neg_sample_path $NEG_SAMPLE_PATH\
+                          --pos_sample_path $POS_SAMPLE_PATH\
+                          --save_dir $TFRECORD_DIR
+                          --k_fold $KFOLD
+```
+Generated tfrecord files would be saved in **TFRECORD_DIR**.
+
+The train.py would search .tfrecord files in the **TFRECORD_DIR** and perform k-fold cross validation.
+k is the number of tfrecords in the directory.
+```
+export CKPT_DIR=<checkpoint_dir> # change this to your favorite directory to save the trained weights
+conda activate tensorflow
+python train.py --record_dir $TFRECORD_DIR\
+                --ckpt_dir CKPT_DIR
 ```
 
 # Performance
--- train.py
+++ train.py
@@ -2,74 +2,88 @@ import os
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 import tensorflow as tf
 import numpy as np
-import time
-import datetime
-import data_helpers
 import argparse
-import preprocessing
 import text_cnn
+from glob import glob
+import shutil
 
-def preprocess(args):
-    x_text, y = data_helpers.load_data_and_labels(args.positive_data_file, args.negative_data_file)
+def decode_tfrecord(record_bytes):
+    feature = tf.io.parse_single_example(
+        record_bytes,
+        {"feature": tf.io.FixedLenFeature([], dtype=tf.string),
+         "label": tf.io.FixedLenFeature([], dtype=tf.string),
+         'd1': tf.io.FixedLenFeature([], dtype=tf.int64),
+         'd2': tf.io.FixedLenFeature([], dtype=tf.int64)}
+    )
+    x = tf.io.decode_raw(feature['feature'], tf.float32)
+    x = tf.reshape(x, (feature['d1'], feature['d2'], 1))
+    y = tf.io.decode_raw(feature['label'], tf.int64)
+    return x, y
 
-    # Build vocabulary
-    max_document_length = max([len(x.split(" ")) for x in x_text])
-    vocab_processor = preprocessing.VocabularyProcessor(max_document_length)
-    x = np.array(list(vocab_processor.fit_transform(x_text)))
-    # Randomly shuffle data
-    np.random.seed(10)
-    shuffle_indices = np.random.permutation(np.arange(len(y)))
-    x_shuffled = x[shuffle_indices]
-    y_shuffled = y[shuffle_indices]
-
-    # Split train/test set
-    # TODO: This is very crude, should use cross-validation
-    dev_sample_index = -1 * int(args.dev_sample_percentage * float(len(y)))
-    x_train, x_dev = x_shuffled[:dev_sample_index], x_shuffled[dev_sample_index:]
-    y_train, y_dev = y_shuffled[:dev_sample_index], y_shuffled[dev_sample_index:]
-    print("Vocabulary Size: {:d}".format(len(vocab_processor.vocabulary_)))
-    print("Train/Dev split: {:d}/{:d}".format(len(y_train), len(y_dev)))
-    return x_train, y_train, vocab_processor, x_dev, y_dev
-
-def create_dataset(x, y, batch_size):
-    dataset = tf.data.Dataset.from_tensor_slices((x, y))
-    dataset = dataset.shuffle(buffer_size=10000).batch(batch_size)
+def dataset_from_tfrecord(tfrecord_files, args):
+    dataset = tf.data.TFRecordDataset(tfrecord_files).map(decode_tfrecord, num_parallel_calls=6).shuffle(1000).batch(args.batch_size)
     return dataset
 
+def train(args):
+    assert os.path.isdir(args.record_dir)
+    shutil.rmtree(args.ckpt_dir)
+    os.makedirs(args.ckpt_dir)
+    tfrecord_files = glob(os.path.join(args.record_dir, '*.tfrecord'))
+    tfrecord_files.sort()
+    test_acc = []
+    print('{} tfrecord files found'.format(tfrecord_files.__len__()))
+    if args.k_fold:
+        num_fold = tfrecord_files.__len__()
+        print('Perform {}-fold cross validation'.format(num_fold))
+        for i_fold in range(num_fold):
+            print('*' * 30 + "start fold {}".format(i_fold) + '*' * 30)
+            # prepare train, validate, test datasets
+            val_idx = i_fold
+            test_idx = (i_fold + 1) % num_fold
+            val_records = [tfrecord_files[val_idx]]
+            test_records = [tfrecord_files[test_idx]]
+            train_records = [tfrecord_files[i] for i in range(num_fold) if i != val_idx and i != test_idx]
+            train_ds = dataset_from_tfrecord(train_records, args)
+            val_ds = dataset_from_tfrecord(val_records, args)
+            test_ds = dataset_from_tfrecord(test_records, args)
+            batch_x, batch_y = next(iter(train_ds))
+            # save best model
+            ckpt_path = os.path.join(args.ckpt_dir, str(i_fold))
+            ckpt_callback = tf.keras.callbacks.ModelCheckpoint(filepath=ckpt_path,
+                                                               save_weights_only=True,
+                                                               monitor='val_loss',
+                                                               mode='min',
+                                                               save_best_only=True)
 
-def train(x_train, y_train, vocab_processor, x_dev, y_dev, args):
-    tf.config.set_soft_device_placement(True)
-    train_ds = create_dataset(x_train, y_train, args.batch_size)
-    val_ds = create_dataset(x_dev, y_dev, args.batch_size)
-    model = text_cnn.create_text_cnn(sequence_length=x_train.shape[1],
-                                     num_classes=y_train.shape[1],
-                                     vocab_size=len(vocab_processor.vocabulary_),
-                                     embedding_size=args.embedding_dim,
-                                     filter_sizes=list(map(int, args.filter_sizes.split(","))),
-                                     num_filters=args.num_filters,
-                                     dropout_keep_prob=args.dropout_keep_prob,
-                                     l2_reg_lambda=args.l2_reg_lambda)
-    model.compile(optimizer=tf.keras.optimizers.Adam(1e-3),
-                  loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
-                  metrics=['accuracy'])
-    model.fit(train_ds, epochs=args.num_epochs, validation_data=val_ds)
+            model = text_cnn.create_text_cnn_pretrained(sequence_length=batch_x.shape[1],
+                                                        num_classes=batch_y.shape[1],
+                                                        embedding_size=300,
+                                                        filter_sizes=list(map(int, args.filter_sizes.split(","))),
+                                                        num_filters=args.num_filters,
+                                                        dropout_keep_prob=args.dropout_keep_prob,
+                                                        l2_reg_lambda=args.l2_reg_lambda)
+            model.compile(optimizer=tf.keras.optimizers.Adam(args.learning_rate, clipnorm=args.l2_norm_clip),
+                          loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
+                          metrics=['categorical_accuracy'])
+            model.fit(train_ds, epochs=args.num_epochs, validation_data=val_ds, callbacks=[ckpt_callback])
+            model.load_weights(ckpt_path)
+            res = model.evaluate(test_ds)
+            test_acc.append(res[1])
+        print('overall test accuracy = {}'.format(np.mean(test_acc)))
+
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dev_sample_percentage', type=float, default=0.1, help="Percentage of the training data to use for validation")
-    parser.add_argument('--positive_data_file', type=str, default='./data/rt-polaritydata/rt-polarity.pos', help='Data source for the positive data.')
-    parser.add_argument('--negative_data_file', type=str, default='./data/rt-polaritydata/rt-polarity.neg', help='Data source for the negative data.')
-    parser.add_argument('--embedding_dim', type=int, default=128, help='Dimensionality of character embedding (default: 128)')
-    parser.add_argument('--filter_sizes', type=str, default="3", help='Comma-separated filter sizes (default: "3,4,5")')
-    parser.add_argument('--num_filters', type=int, default=128, help='Number of filters per filter size (default: 128)')
-    parser.add_argument('--dropout_keep_prob', type=float, default=0.5, help="Dropout keep probability (default: 0.5)")
-    parser.add_argument('--l2_reg_lambda', type=float, default=0.0, help="L2 regularization lambda (default: 0.0)")
+    parser.add_argument('--record_dir', type=str, default='tfrecord/rt-polarity', help="path to the tfrecords of the dataset")
+    parser.add_argument('--ckpt_dir', type=str, default='checkpoint')
+    parser.add_argument('--learning_rate', type=float, default=1e-4, help="learning rate")
     parser.add_argument('--batch_size', type=int, default=512, help="Batch Size (default: 64)")
     parser.add_argument('--num_epochs', type=int, default=200, help="Number of training epochs (default: 200)")
-    parser.add_argument('--evaluate_every', type=int, default=100, help="Evaluate model on dev set after this many steps (default: 100)")
-    parser.add_argument('--checkpoint_every', type=int, default=100, help="Save model after this many steps (default: 100)")
-    parser.add_argument('--num_checkpoints', type=int, default=5, help="Number of checkpoints to store (default: 5)")
+    parser.add_argument('--filter_sizes', type=str, default="3,4,5", help='Comma-separated filter sizes (default: "3,4,5")')
+    parser.add_argument('--num_filters', type=int, default=100, help='Number of filters per filter size (default: 128)')
+    parser.add_argument('--dropout_keep_prob', type=float, default=0.5, help="Dropout keep probability (default: 0.5)")
+    parser.add_argument('--l2_reg_lambda', type=float, default=0.1, help="L2 regularization lambda (default: 0.0)")
+    parser.add_argument('--l2_norm_clip', type=float, default=3, help="constrain of l2-norm of the weight vectors")
     args = parser.parse_args()
-    x_train, y_train, vocab_processor, x_dev, y_dev = preprocess(args)
-    train(x_train, y_train, vocab_processor, x_dev, y_dev, args)
+    train(args)
