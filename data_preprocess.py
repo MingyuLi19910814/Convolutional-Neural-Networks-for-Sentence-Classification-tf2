d0485b42ee7795b14c3d9d1814bd5d08381b937f
Mingyu Li <mingyuli19910814@gmail.com>
2022-05-01 18:18:05 +0900

    k-fold training with tfrecord is working

--- data_preprocess.py
+++ data_preprocess.py
@@ -4,19 +4,24 @@ import tensorflow as tf
 import argparse
 import data_helpers
 import numpy as np
-import pathlib
 import random
 from gensim import downloader
 import re
 from tqdm import tqdm
+from sklearn.model_selection import KFold
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--neg_sample_path', type=str, default='data/rt-polaritydata/rt-polarity.neg')
 parser.add_argument('--pos_sample_path', type=str, default='data/rt-polaritydata/rt-polarity.pos')
-parser.add_argument('--save_path', type=str, default='tfrecord/rt-polarity.tfrecord')
+parser.add_argument('--save_dir', type=str, default='tfrecord/rt-polarity')
 parser.add_argument('--max_document_len', type=int, default=-1)
+parser.add_argument('--k_fold', type=int, default=10)
 
 tokenizer = re.compile(r"[A-Z]{2,}(?![a-z])|[A-Z][a-z]+(?=[A-Z])|[\'\w\-]+", re.UNICODE)
+
+def _int64_feature(value):
+    return tf.train.Feature(int64_list=tf.train.Int64List(value=[value]))
 
 def _bytes_feature(value):
     return tf.train.Feature(bytes_list=tf.train.BytesList(value=[value]))
@@ -29,6 +34,7 @@ def encode_sentence(sentence, label, vocabulary, max_document_len):
     :param max_document_len: maximum word number in a sentence
     :return:
     '''
+    print('label shape = {}, dtype = {}'.format(label.shape, label.dtype))
     encoded_feature = np.zeros(shape=(max_document_len, 300), dtype=np.float32)
     for idx, word in enumerate(tokenizer.findall(sentence)):
         if idx >= max_document_len:
@@ -37,7 +43,9 @@ def encode_sentence(sentence, label, vocabulary, max_document_len):
             encoded_feature[idx] = vocabulary[word]
     feature = {
         'feature': _bytes_feature(encoded_feature.tobytes()),
-        'label': _bytes_feature(label.tobytes())
+        'label': _bytes_feature(label.tobytes()),
+        'd1': _int64_feature(encoded_feature.shape[0]),
+        'd2': _int64_feature(encoded_feature.shape[1])
     }
     msg = tf.train.Example(features=tf.train.Features(feature=feature))
     return msg
@@ -55,13 +63,15 @@ def main(args):
     #load google news 300 vocabulary
     vocabulary = downloader.load('word2vec-google-news-300')
 
-    #encode
-    save_dir = pathlib.Path(args.save_path).parent
-    os.makedirs(save_dir, exist_ok=True)
-    with tf.io.TFRecordWriter(args.save_path) as writer:
-        for sentence, label in tqdm(zip(sentences, labels)):
-            msg = encode_sentence(sentence, label, vocabulary, max_document_len)
-            writer.write(msg.SerializeToString())
+    # perform k-fold split on the samples and encode
+    os.makedirs(args.save_dir, exist_ok=True)
+    kfold = KFold(args.k_fold)
+    for k, (_, sample_indexes) in enumerate(kfold.split(np.arange(sentences.__len__()))):
+        save_path = os.path.join(args.save_dir, 'fold-' + str(k) + '.tfrecord')
+        with tf.io.TFRecordWriter(save_path) as writer:
+            for idx in sample_indexes:
+                msg = encode_sentence(sentences[idx], labels[idx], vocabulary, max_document_len)
+                writer.write(msg.SerializeToString())
 
 if __name__ == "__main__":
     args = parser.parse_args()
--- transfer_learning.py
+++ transfer_learning.py
@@ -8,75 +8,91 @@ import data_helpers
 import argparse
 import preprocessing
 import text_cnn
-from gensim import downloader
 from sklearn.model_selection import train_test_split
 import re
+from glob import glob
 
 
-def create_wordvec_dataset(X, Y, vocabulary, batch_size, max_document_len):
-    tokenizer = re.compile(r"[A-Z]{2,}(?![a-z])|[A-Z][a-z]+(?=[A-Z])|[\'\w\-]+", re.UNICODE)
-    def word2vec_process(x, y):
-        word_vecs = np.zeros(shape=(max_document_len, 300), dtype=np.float32)
-        x = x.numpy().decode('utf-8')
-        for idx, word in enumerate(tokenizer.findall(x)):
-            if idx >= max_document_len:
-                break
-            if word in vocabulary:
-                word_vecs[idx] = vocabulary[word]
-        return word_vecs, tf.cast(y, tf.float32)
-    dataset = tf.data.Dataset.from_tensor_slices((X, Y))
-    dataset = dataset.map(lambda x, y: tf.py_function(word2vec_process, inp=[x, y], Tout=[np.float32, np.float32]),
-                          num_parallel_calls=tf.data.experimental.AUTOTUNE)
-    dataset = dataset.shuffle(buffer_size=10000).batch(batch_size)
+def decode_tfrecord(record_bytes):
+    feature = tf.io.parse_single_example(
+        record_bytes,
+        {"feature": tf.io.FixedLenFeature([], dtype=tf.string),
+         "label": tf.io.FixedLenFeature([], dtype=tf.string),
+         'd1': tf.io.FixedLenFeature([], dtype=tf.int64),
+         'd2': tf.io.FixedLenFeature([], dtype=tf.int64)}
+    )
+    x = tf.io.decode_raw(feature['feature'], tf.float32)
+    x = tf.reshape(x, (feature['d1'], feature['d2'], 1))
+    y = tf.io.decode_raw(feature['label'], tf.int64)
+    return x, y
+
+def dataset_from_tfrecord(tfrecord_files, args):
+    dataset = tf.data.TFRecordDataset(tfrecord_files).map(decode_tfrecord).shuffle(1000).batch(args.batch_size)
     return dataset
 
-def preprocess(args):
-    X, Y = data_helpers.load_data_and_labels(args.positive_data_file, args.negative_data_file)
-    max_document_length = max([len(x.split(" ")) for x in X])
-    train_x, val_x, train_y, val_y = train_test_split(X, Y, test_size=args.dev_sample_percentage)
-    print('loading Google News 300 Word2vec...')
-    google_vocabulary = downloader.load('word2vec-google-news-300')
-    print('creating datasets')
-    train_ds = create_wordvec_dataset(train_x, train_y, google_vocabulary, args.batch_size, max_document_length)
-    val_ds = create_wordvec_dataset(val_x, val_y, google_vocabulary, args.batch_size, max_document_length)
-    return train_ds, val_ds
+def train(args):
+    assert os.path.isdir(args.record_dir)
+    os.makedirs(args.ckpt_dir)
+    tfrecord_files = glob(os.path.join(args.record_dir, '*.tfrecord'))
+    tfrecord_files.sort()
+    print('{} tfrecord files found'.format(tfrecord_files.__len__()))
+    if args.k_fold:
+        num_fold = tfrecord_files.__len__()
+        print('Perform {}-fold cross validation'.format(num_fold))
+        for i_fold in range(num_fold):
+            print('*' * 30 + "start fold {}".format(i_fold) + '*' * 30)
+            # prepare train, validate, test datasets
+            val_idx = i_fold
+            test_idx = (i_fold + 1) % num_fold
+            val_records = [tfrecord_files[val_idx]]
+            test_records = [tfrecord_files[test_idx]]
+            train_records = [tfrecord_files[i] for i in range(num_fold) if i != val_idx and i != test_idx]
+            train_ds = dataset_from_tfrecord(train_records, args)
+            val_ds = dataset_from_tfrecord(val_records, args)
+            test_ds = dataset_from_tfrecord(test_records, args)
+            batch_x, batch_y = next(iter(train_ds))
+            # save best model
+            ckpt_path = os.path.join(args.ckpt_dir, str(i_fold))
+            ckpt_callback = tf.keras.callbacks.ModelCheckpoint(filepath=ckpt_path,
+                                                               save_weights_only=True,
+                                                               monitor='val_loss',
+                                                               mode='min',
+                                                               save_best_only=True)
 
-def create_dataset(x, y, batch_size):
-    dataset = tf.data.Dataset.from_tensor_slices((x, y))
-    dataset = dataset.batch(batch_size)
-    return dataset
+            model = text_cnn.create_text_cnn_pretrained(sequence_length=batch_x.shape[1],
+                                                        num_classes=batch_y.shape[1],
+                                                        embedding_size=300,
+                                                        filter_sizes=list(map(int, args.filter_sizes.split(","))),
+                                                        num_filters=args.num_filters,
+                                                        dropout_keep_prob=args.dropout_keep_prob,
+                                                        l2_reg_lambda=args.l2_reg_lambda)
+            model.compile(optimizer=tf.keras.optimizers.Adam(args.learning_rate),
+                          loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
+                          metrics=['categorical_accuracy'])
+            model.fit(train_ds, epochs=args.num_epochs, validation_data=val_ds, callbacks=[ckpt_callback])
+            model.load_weights(ckpt_path)
+            res = model.evaluate(test_ds)
+            print('res = {}'.format(res))
 
 
-def train(train_ds, val_ds, args):
-    tf.config.set_soft_device_placement(True)
-    batch_x, batch_y = next(iter(train_ds))
-    model = text_cnn.create_text_cnn_pretrained(sequence_length=batch_x.shape[1],
-                                                num_classes=batch_y.shape[1],
-                                                embedding_size=300,
-                                                filter_sizes=list(map(int, args.filter_sizes.split(","))),
-                                                num_filters=args.num_filters,
-                                                dropout_keep_prob=args.dropout_keep_prob,
-                                                l2_reg_lambda=args.l2_reg_lambda)
-    model.compile(optimizer=tf.keras.optimizers.Adam(args.learning_rate),
-                  loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
-                  metrics=['categorical_accuracy'])
-    model.fit(train_ds, epochs=args.num_epochs, validation_data=val_ds)
+    else:
+        pass
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
+    parser.add_argument('--record_dir', type=str, default='tfrecord/rt-polarity', help="path to the tfrecords of the dataset")
+    parser.add_argument('--k_fold',
+                        type=bool,
+                        default=True,
+                        help="whether perform k-fold cross validation. If true, k would be equal to the number of tfrecord in the record_dir")
+    parser.add_argument('--ckpt_dir', type=str, default='checkpoint')
     parser.add_argument('--learning_rate', type=float, default=1e-3, help="learning rate")
     parser.add_argument('--batch_size', type=int, default=512, help="Batch Size (default: 64)")
     parser.add_argument('--num_epochs', type=int, default=30, help="Number of training epochs (default: 200)")
-    parser.add_argument('--use_google_news_300', type=bool, default=True, help="if use word2vec vocabulary pretrained on google news")
-    parser.add_argument('--dev_sample_percentage', type=float, default=0.1, help="Percentage of the training data to use for validation")
-    parser.add_argument('--positive_data_file', type=str, default='./data/rt-polaritydata/rt-polarity.pos', help='Data source for the positive data.')
-    parser.add_argument('--negative_data_file', type=str, default='./data/rt-polaritydata/rt-polarity.neg', help='Data source for the negative data.')
-    parser.add_argument('--embedding_dim', type=int, default=128, help='Dimensionality of character embedding (default: 128)')
     parser.add_argument('--filter_sizes', type=str, default="3", help='Comma-separated filter sizes (default: "3,4,5")')
     parser.add_argument('--num_filters', type=int, default=128, help='Number of filters per filter size (default: 128)')
     parser.add_argument('--dropout_keep_prob', type=float, default=0.5, help="Dropout keep probability (default: 0.5)")
     parser.add_argument('--l2_reg_lambda', type=float, default=0.0, help="L2 regularization lambda (default: 0.0)")
     args = parser.parse_args()
-    train_ds, val_ds = preprocess(args)
-    train(train_ds, val_ds, args)
+    train(args)
